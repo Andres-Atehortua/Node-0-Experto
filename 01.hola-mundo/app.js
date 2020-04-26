cc2921621a49a2161a7000164fef6f5a88f2c532
Andres Atehortua <anderson17colombia@hotmail.com>
2020-04-26 16:14:15 +0200

    [ADD] Pequeños fragmentos de código para comenzar.

---
+++ 01.hola-mundo/app.js
@@ -0,0 +1,5 @@
+let mundo = "Mundo";
+console.log(`Hola ${mundo}`);
+
+// Ejecutar node app.js en la terminal.
+// Ejecutar nodemon app.js en la terminal.
---
+++ 01.hola-mundo/app2.js
@@ -0,0 +1,8 @@
+function saludar(nombre) {
+  let mensaje = `Hola ${nombre}`;
+  return mensaje;
+}
+
+let saludo = saludar("Andrés");
+
+console.log(saludo);
