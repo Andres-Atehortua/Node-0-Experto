de99fbb6f622b28124c450dc10a71c66a66d7fe9
Andres Atehortua <anderson17colombia@hotmail.com>
2020-04-27 19:52:27 +0200

    [ADD] Añadidos cambios para mejorar la aplicación.

--- 03.bases-node/app.js
+++ 03.bases-node/app.js
@@ -1,11 +1,26 @@
-const { crearArchivo } = require("./multiplicar");
+const argv = require("./config/yargs.config");
 
-// Para que esto funcione hay que introducir en la terminal: node app --base=5
-let argv = process.argv[2].split("=")[1];
-let base = argv;
+const { crearArchivo, listarTabla } = require("./multiplicar");
 
-console.log(base);
+// Para que esta configuración manual funcione hay que introducir en la terminal: node app --base=5
+// let argv = process.argv[2].split("=")[1];
+// let base = argv;
 
-crearArchivo(base)
-  .then((archivo) => console.log(`Archivo creado con el nombre ${archivo}`))
-  .catch((error) => console.log(error));
+// Utilizando la configuracion de yargs podemos ahora introducir: node app listar -b 5
+// o también: node app listar --base 5. De esta forma accedemos al valor con argv.base o argv.b
+
+let comando = argv._[0];
+
+switch (comando) {
+  case "listar":
+    listarTabla(argv.base, argv.limite);
+    break;
+  case "crear":
+    crearArchivo(argv.base, argv.limite)
+      .then((archivo) => console.log(`Archivo creado con el nombre ${archivo}`))
+      .catch((error) => console.log(error));
+    break;
+  default:
+    console.log("Comando no reconocido");
+    break;
+}
