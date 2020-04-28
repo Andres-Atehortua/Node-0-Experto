7be5dc1f8065ca43014eb6ef2d8774f0acc88b2b
Andres Atehortua <anderson17colombia@hotmail.com>
2020-04-28 22:13:26 +0200

    [ADD] Dependencia de axios añadida y su configuración para hacer peticiones.

--- 05.Clima-App/app.js
+++ 05.Clima-App/app.js
@@ -1,3 +1,17 @@
+const axios = require("axios");
 const argv = require("./config/yargs.config");
 
-console.log(argv.direccion);
+let encodedURL = encodeURI(argv.direccion);
+console.log(encodedURL);
+
+const instance = axios.create({
+  baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedURL}`,
+  headers: {
+    "X-RapidAPI-Key": "c9193e237bmshe9703bf09bd9605p1abc67jsn0fbe5b30a1c9",
+  },
+});
+
+instance
+  .get()
+  .then((response) => console.log(response.data.Results[0]))
+  .catch((err) => console.log(err));
