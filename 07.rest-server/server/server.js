19d619fc9bcfe8b97f195d68242685c48d717452
Andres Atehortua <anderson17colombia@hotmail.com>
2020-05-02 17:58:42 +0200

    [ADD] Actualizado server.

--- 07.rest-server/server/server.js
+++ 07.rest-server/server/server.js
@@ -1,29 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
 const bodyParser = require("body-parser");
 require("dotenv").config();
 //app.use suelen ser middlewares
+
+//Esto es para poder acceder al req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.json("Hey!"));
-app.get("/user", (req, res) => res.json("get user"));
-app.post("/user", (req, res) => {
-  let { name, username, age } = req.body;
-  if (name && username && age) res.json({ name, username, age });
-  else
-    res.status(400).json({ ok: false, message: "You have to send all info." });
-});
-app.put("/user/:id", (req, res) => {
-  let { id } = req.params;
-  res.json({ id });
-});
-app.delete("/user", (req, res) => res.json("delete user"));
-// app.get("/", (req, res) => res.json("Hey!"));
-// app.get("/", (req, res) => res.json("Hey!"));
-// app.get("/", (req, res) => res.json("Hey!"));
+app.use("/", require("./routes/user.route"));
 
-console.log(process.env);
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((x) =>
+    console.log(
+      `Conectado a mongo! nombre de la base de datos: "${x.connections[0].name}"`
+    )
+  )
+  .catch((err) =>
+    console.log("No se ha podido conectar a la base de datos: ", err)
+  );
 
 app.listen(process.env.PORT, () =>
   console.log(`Servidor escuchando en el puerto ${process.env.PORT}`)
