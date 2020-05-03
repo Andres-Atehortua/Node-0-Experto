6cb31941d39bb9f5e536b7e1cbb65a5d12864083
Andres Atehortua <anderson17colombia@hotmail.com>
2020-05-03 18:56:36 +0200

    [ADD] Dependencia jsonwebtoken añadida.

--- 07.rest-server/server/routes/login.routes.js
+++ 07.rest-server/server/routes/login.routes.js
@@ -4,7 +4,24 @@ const router = express.Router();
 const Usuario = require("./../models/user.model");
 
 router.post("/login", (req, res) => {
-  res.json({ ok: true });
+  let { email, password } = req.body;
+
+  Usuario.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(400)
+          .json({ ok: false, err: "(Usuario) o contraseña incorrectos" });
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res
+          .status(400)
+          .json({ ok: false, err: "Usuario o (contraseña) incorrectos" });
+      }
+      res.json({ ok: true, user, token: "123" });
+    })
+    .catch((err) => res.status(500).json({ ok: false, err }));
 });
 
 module.exports = router;
